cad3ba6dddd283a29243d8c9a784de6f8df6b193
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2025-08-31 02:56:55 +0800

    Remove prints

--- photogallery/pythonFlaskServer/FlaskTechJam2025.py
+++ photogallery/pythonFlaskServer/FlaskTechJam2025.py
@@ -14,11 +14,8 @@ def home():
 
 @app.route('/detect', methods=['POST'])
 def detect():
-    print("I have a request at /detect!")
     data = request.get_json()
-    print(data)
     source = data.get("image")
-    print(source, type(source))
     label = data.get("label")
     img = cv2.imread(source)
     h, w = img.shape[:2]
@@ -30,20 +27,8 @@ def detect():
     class_ids = results.boxes.cls.tolist()   # Class indices
 
     for box, class_id in zip(boxes, class_ids):
-        # Don't filter by label yet, return all 
-        print("class: ", class_id)
-        print("Hey returning!")
         x1, y1, x2, y2 = map(float, box)
-        # detections.append({
-        #     "class_id": int(class_id),
-        #     "bbox": [x1 / w,
-        #          y1 / h,
-        #          x2 / w,
-        #          y2 / h]
-        # })
-            
         if label == int(class_id):
-            print("Hey returning!")
             x1, y1, x2, y2 = map(float, box)
             detections.append({
                 "class_id": int(class_id),
@@ -54,22 +39,22 @@ def detect():
             })
 
     json_output = json.dumps(detections, indent=2)
-    print(f"im returning ${json_output}")
     return json_output
 
 
 @app.route('/update_data', methods=['POST'])
 def update_data():
-    print("I have a request at /update_data!")
     data = request.get_json()
-    print(data)
     labels = data.get("labels")
     bbox = data.get("new_bbs")
     path = data.get("image")
 
     if labels is None or bbox is None or path is None:
         return {"error": "Missing required fields"}, 400
-        
+
+    # While this is just overwriting the file, in a real application
+    # you might want to append or modify existing data, or store it in
+    # a more durable way (like a database) 
     with open("new_training_data.txt", mode="w+") as td:
         for idx, label in enumerate(labels):
             entry = f"{label} {bbox[idx][0]} {bbox[idx][1]} {bbox[idx][2]} {bbox[idx][3]}\n"
